238a3bd63ae96acae5b0699fb0acde2d21f68f07
martinaRoncati <martina.roncati@capellinisauro.edu.it>
2025-04-24 07:27:46 +0000

    Commit iniziale

--- Hello.java
+++ Hello.java
@@ -3,5 +3,14 @@ public class Hello
     public static void main(String[] args) 
     {
         System.out.println("hello martina!");
+
+        //stampo 3 volte il nome 
+        for (int i = 0; i < 3; i++)
+        {
+            System.out.println("martina");
+        }
+
+        //stampo la mia data di nascita
+        System.out.println("13/08/2007");
     }
 }
