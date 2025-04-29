b58e2034240efde2268bf5a1a4da05d1f500a4ab
martinaRoncati <martina.roncati@capellinisauro.edu.it>
2025-04-29 18:34:56 +0200

    stampo il mio nome 3 volte

--- Hello.java
+++ Hello.java
@@ -3,5 +3,10 @@ public class Hello
     public static void main(String[] args) 
     {
         System.out.println("hello martina!");
+
+        //stampo il mio nome 3 volte
+        for (int i=0; i<3; i++)
+        {
+            System.out.println("martina");
     }
 }
